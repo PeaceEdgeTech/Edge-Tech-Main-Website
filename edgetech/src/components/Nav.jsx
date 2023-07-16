4211e8b56c7d5c41d817cd2670fa763a2bd5a65f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-07-16 20:18:42 +0000

    ...

--- edgetech/src/components/Nav.jsx
+++ edgetech/src/components/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
   };
 
   return (
-    <nav className="bg-black text-blue-600">
+    <nav className="bg-black text-blue-600 font-bold">
       <div className="container mx-auto px-4 py-2 flex items-center justify-between">
         <div className="ml-0">
         <img src={logo} alt="..." className='w-20 h-20'/>
@@ -26,26 +26,40 @@ const Nav = () => {
           </button>
         </div>
 
-        <div className="hidden lg:flex space-x-4">
-          <a href="/" className="hover:text-gray-400">Home</a>
-          <a href="/about" className="hover:text-gray-400">About</a>
-          <a href="/skills" className="hover:text-gray-400">Skills</a>
-          <a href="/projects" className="hover:text-gray-400">Projects</a>
-          <a href="/team" className="hover:text-gray-400">Team</a>
-          <button>
-          <a href="/contact" className="hover:text-gray-400">Contact</a>
-          </button>
+        <div className="hidden lg:flex space-x-5 items-center">
+          <ul className="lg:flex space-x-5 items-center">
+            <li>
+              <a href="/" className="hover:text-blue-400">Home</a>
+            </li>
+            <li>
+              <a href="/about" className="hover:text-blue-400">About</a>
+            </li>
+            <li>
+              <a href="/skills" className="hover:text-blue-400">Skills</a>
+            </li>
+            <li>
+              <a href="/projects" className="hover:text-blue-400">Projects</a>
+            </li>
+            <li>
+              <a href="/team" className="hover:text-blue-400">Team</a>
+            </li>
+            <li>
+              <button className="bg-gray-900 rounded p-2 w-25 border border-blue-600 hover:bg-gray-700 hover:text-blue-400">
+                <a href="/contact">Contact</a>
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
 
       {isOpen && (
         <div className="lg:hidden py-2 px-4">
-          <a href="/" className="hover:text-gray-400 block py-1">Home</a>
-          <a href="/about" className="hover:text-gray-400 block py-1">About</a>
-          <a href="/skills" className="hover:text-gray-400 block py-1">Skills</a>
-          <a href="/projects" className="hover:text-gray-400 block py-1">Projects</a>
-          <a href="/team" className="hover:text-gray-400 block py-1">Team</a>
-          <a href="/contact" className="hover:text-gray-400 block py-1">Contact</a>
+          <a href="/" className="hover:text-blue-400 block py-1">Home</a>
+          <a href="/about" className="hover:text-blue-400 block py-1">About</a>
+          <a href="/skills" className="hover:text-blue-400 block py-1">Skills</a>
+          <a href="/projects" className="hover:text-blue-400 block py-1">Projects</a>
+          <a href="/team" className="hover:text-blue-400 block py-1">Team</a>
+          <a href="/contact" className="hover:text-blue-400 block py-1">Contact</a>
         </div>
       )}
     </nav>
