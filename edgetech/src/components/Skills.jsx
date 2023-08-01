2ed03154bd82c51740989ce30a2f4e7414737299
AYG3 <gilbertayoku3@.com>
2023-08-01 23:07:57 +0100

    skills in progress

--- edgetech/src/components/Skills.jsx
+++ edgetech/src/components/Skills.jsx
@@ -8,95 +8,104 @@ const Skills = () => {
         <h3 className="font-semibold text-4xl text-blue-600">SKILLS</h3>
       </header>
 
-      <div className="container mx-auto py-8 text-gray-700">
-        <div className="flex justify-center">
-          <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4">
+      <div className="container mx-auto ">
+        <div className="flex flex-col space-y-16">
 
-            
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">HTML</div>
-              </div>
-            </article>
+            {/* FIRST ROW */}
+            <div className='flex ring-1 p-4 mx-4 rounded-lg transition translate-x-12'>
 
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">CSS</div>
-              </div>
-            </article>
+              <article className="">
+                <div className="flex p-2">
+                  <FaDotCircle className=" mr-1 relative top-1" />
+                  <div className="font-semibold">HTML</div>
+                </div>
+              </article>
 
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">Bootstrap</div>
-              </div>
-            </article>
+              <article className="">
+                <div className="flex p-2">
+                  <FaDotCircle className="mr-1 relative top-1" />
+                  <div className="font-semibold">CSS</div>
+                </div>
+              </article>
 
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">SASS</div>
-              </div>
-            </article>
+              <article className="">
+                <div className="flex p-2">
+                  <FaDotCircle className="mr-1 relative top-1" />
+                  <div className="font-semibold">Bootstrap</div>
+                </div>
+              </article>
 
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">Tailwind CSS</div>
-              </div>
-            </article>
+              <article className="">
+                <div className="flex p-2">
+                  <FaDotCircle className="mr-1 relative top-1" />
+                  <div className="font-semibold">SASS</div>
+                </div>
+              </article>
 
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">JavaScript</div>
-              </div>
-            </article>
+              <article className="">
+                <div className="flex p-2">
+                  <FaDotCircle className="mr-1 relative top-1" />
+                  <div className="font-semibold">Tailwind CSS</div>
+                </div>
+              </article>
 
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">PHP</div>
-              </div>
-            </article>
-
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">LARAVEL</div>
-              </div>
-            </article>
-
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">MongoDB</div>
-              </div>
-            </article>
-
-            <div className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">SQLite</div>
-              </div>
+              <article className="">
+                <div className="flex p-2">
+                  <FaDotCircle className="mr-1 relative top-1" />
+                  <div className="font-semibold">JavaScript</div>
+                </div>
+              </article>
             </div>
 
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">Python</div>
-              </div>
-            </article>
 
-            <article className="p-4 border border-blue-500 rounded-lg transition duration-300 hover:bg-black hover:text-blue-500">
-              <div className="flex items-center">
-                <FaDotCircle className="text-blue-500 mr-2" />
-                <div className="font-semibold">Django</div>
-              </div>
-            </article>
-          </div>
+
+
+              {/* SECOND ROW */}
+            <div className='flex ring-1 p-4 mx-4 rounded-xl'>
+
+                <article className="">
+                  <div className=" flex p-2">
+                    <FaDotCircle className="mr-1 relative top-1" />
+                    <div className="font-semibold">PHP</div>
+                  </div>
+                </article>
+
+                <article className="">
+                  <div className="flex p-2">
+                    <FaDotCircle className="mr-1 relative top-1" />
+                    <div className="font-semibold">LARAVEL</div>
+                  </div>
+                </article>
+
+                <article className="">
+                  <div className="flex p-2">
+                    <FaDotCircle className="mr-1 relative top-1" />
+                    <div className="font-semibold">MongoDB</div>
+                  </div>
+                </article>
+
+                <div className="">
+                  <div className="flex p-2">
+                    <FaDotCircle className="mr-1 relative top-1" />
+                    <div className="font-semibold">SQLite</div>
+                  </div>
+                </div>
+
+                <article className="">
+                  <div className="flex p-2">
+                    <FaDotCircle className="mr-1 relative top-1" />
+                    <div className="font-semibold">Python</div>
+                  </div>
+                </article>
+
+                <article className="">
+                  <div className="flex p-2">
+                    <FaDotCircle className="mr-1 relative top-1" />
+                    <div className="font-semibold">Django</div>
+                  </div>
+                </article>
+
+            </div>
         </div>
       </div>
     </div>
