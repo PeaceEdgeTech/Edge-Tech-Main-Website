5a304c37a69c8ad688435016a63fe0570257e434
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-08-07 10:55:41 +0100

    Update Services.jsx

--- edgetech/src/components/Services.jsx
+++ edgetech/src/components/Services.jsx
@@ -8,17 +8,17 @@ const Services = () => {
       <h3 className="text-4xl tracking-widest uppercase text-blue-600 font-semibold">Services</h3>
     </header>
     <div className='grid grid-col-1 sm:grid-cols-3 md:grid-cols-3 items-center justify-center p-4'>
-        <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2'>
+        <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2 sm:mb-2'>
             <h3 className='uppercase text-xl text-blue-800 mb-1'>Web Development</h3>
             <img src={audi} alt="" className='rounded-full w-20 h-20 m-1 cursor-pointer'/>
             <p className='text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas delectus odio aperiam recusandae, architecto quibusdam vero obcaecati quae aliquam quaerat laboriosam doloribus temporibus culpa illum autem adipisci omnis nostrum!</p>
         </div>
-        <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2'>
+        <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2 sm:mb-2'>
             <h3 className='uppercase text-xl text-blue-800 mb-1'>Software Development</h3>
             <img src={audi} alt="" className='rounded-full w-20 h-20 m-1 cursor-pointer'/>
             <p className='text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas delectus odio aperiam recusandae, architecto quibusdam vero obcaecati quae aliquam quaerat laboriosam doloribus temporibus culpa illum autem adipisci omnis nostrum!</p>
         </div>
-        <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2'>
+        <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2 sm:mb-2'>
             <h3 className='uppercase text-xl text-blue-800 mb-1'>Mobile Development</h3>
             <img src={audi} alt="" className='rounded-full w-20 h-20 m-1 cursor-pointer'/>
             <p className='text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas delectus odio aperiam recusandae, architecto quibusdam vero obcaecati quae aliquam quaerat laboriosam doloribus temporibus culpa illum autem adipisci omnis nostrum!</p>
