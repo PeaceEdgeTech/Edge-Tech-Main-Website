a023638b22edfff04cb08b63a35d77e5288f3c3e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-08-07 11:05:48 +0100

    Update Services.jsx

--- edgetech/src/components/Services.jsx
+++ edgetech/src/components/Services.jsx
@@ -11,17 +11,17 @@ const Services = () => {
         <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2'>
             <h3 className='uppercase text-xl text-blue-800 mb-1'>Web Development</h3>
             <img src={audi} alt="" className='rounded-full w-20 h-20 m-1 cursor-pointer'/>
-            <p className='text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas delectus odio aperiam recusandae, architecto quibusdam vero obcaecati quae aliquam quaerat laboriosam doloribus temporibus culpa illum autem adipisci omnis nostrum!</p>
+            <p className='text-gray-800'>Our web development services focus on creating dynamic and user-friendly websites that align with your business objectives. From crafting visually appealing designs to implementing robust functionalities, we utilize the latest technologies and industry best practices to ensure your online presence stands out and delivers an exceptional user experience.</p>
         </div>
         <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2'>
             <h3 className='uppercase text-xl text-blue-800 mb-1'>Software Development</h3>
             <img src={audi} alt="" className='rounded-full w-20 h-20 m-1 cursor-pointer'/>
-            <p className='text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas delectus odio aperiam recusandae, architecto quibusdam vero obcaecati quae aliquam quaerat laboriosam doloribus temporibus culpa illum autem adipisci omnis nostrum!</p>
+            <p className='text-gray-800'>With our software development services, we bring your innovative ideas to life through custom software solutions. Our team of skilled developers designs and builds tailored applications, addressing specific business needs and challenges. We prioritize scalability, security, and efficiency to help your organization stay ahead in a fast-paced digital landscape.</p>
         </div>
         <div className='flex flex-col items-center justify-center p-2 shadow-xl shadow-blue-400 rounded-lg mx-2'>
             <h3 className='uppercase text-xl text-blue-800 mb-1'>Mobile Development</h3>
             <img src={audi} alt="" className='rounded-full w-20 h-20 m-1 cursor-pointer'/>
-            <p className='text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas delectus odio aperiam recusandae, architecto quibusdam vero obcaecati quae aliquam quaerat laboriosam doloribus temporibus culpa illum autem adipisci omnis nostrum!</p>
+            <p className='text-gray-800'>Our mobile app development services cater to the ever-growing mobile market. We specialize in creating intuitive and feature-rich mobile applications for Android and iOS platforms. Whether you need a consumer-focused app or an enterprise-level solution, we take a user-centric approach to ensure your app engages and delights users while driving business growth.</p>
         </div>
     </div>
     </div>
